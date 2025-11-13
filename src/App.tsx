8f8555631eeeea9bc88f00241397b84ee20633f0
summit1123 <ruggy245@naver.com>
2025-11-14 01:41:19 +0900

    Initial React TypeScript project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Amy Avatar</h1>
+      <p>아바타 화면을 위한 준비 완료</p>
+    </div>
+  );
+}
+
+export default App;
